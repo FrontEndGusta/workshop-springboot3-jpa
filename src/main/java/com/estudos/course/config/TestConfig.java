3b3c646dbadef596d6d19a0b9953237c1e02f084
Gustavo Silva De Lima <gustavo.lima@cea.com.br>
2024-09-30 22:16:10 -0300

    OrderStatus enum

--- src/main/java/com/estudos/course/config/TestConfig.java
+++ src/main/java/com/estudos/course/config/TestConfig.java
@@ -2,6 +2,7 @@ package com.estudos.course.config;
 
 import com.estudos.course.entities.Order;
 import com.estudos.course.entities.User;
+import com.estudos.course.enums.OrderStatus;
 import com.estudos.course.repository.OrderRepository;
 import com.estudos.course.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -28,9 +29,9 @@ public class TestConfig implements CommandLineRunner {
         User u1 = new User(null, "Maria Brown", "maria@gmail.com", "988888888", "123456");
         User u2 = new User(null, "Alex Green", "alex@gmail.com", "977777777", "123456");
 
-        Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), u1);
-        Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), u2);
-        Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), u1);
+        Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), OrderStatus.PAID, u1);
+        Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), OrderStatus.CANCELED, u2);
+        Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), OrderStatus.DELIVERED, u1);
 
 
         userRepository.saveAll(Arrays.asList(u1, u2));
